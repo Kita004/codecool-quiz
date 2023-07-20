02fd6b80025a845e03d3a58aa911619738c166ed
Kita004 <born.kitty97@gmail.com>
2023-07-20 15:46:12 +0200

    improve landing page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,19 @@
-import {MdOutlineKeyboardDoubleArrowLeft, MdOutlineKeyboardDoubleArrowRight} from "react-icons/md";
+import {
+  MdOutlineKeyboardDoubleArrowLeft,
+  MdOutlineKeyboardDoubleArrowRight,
+} from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
-    const nextPage = "/quiz";
-    const nav = useNavigate();
+  const greetingMsg = "* insert greeting message *";
+  const nextPage = "/quiz";
+  const nav = useNavigate();
 
-    return <div onClick={() => nav(nextPage)} id="greeting-msg" className="flex-container greeting-msg">
-        <MdOutlineKeyboardDoubleArrowLeft className="decorative-arrow"/>
-        <h1>Tell My Future!</h1>
-        <MdOutlineKeyboardDoubleArrowRight className="decorative-arrow"/>
+  return (
+    <div id="home-page" className="flex-container greeting-msg">
+      <MdOutlineKeyboardDoubleArrowLeft className="decorative-arrow" />
+      <h1 onClick={() => nav(nextPage)}>{greetingMsg}</h1>
+      <MdOutlineKeyboardDoubleArrowRight className="decorative-arrow" />
     </div>
-}
+  );
+};

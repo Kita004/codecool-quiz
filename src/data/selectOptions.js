740bd86a4213571ca90d6ed0abcf7ae9133f586a
Kita004 <born.kitty97@gmail.com>
2023-07-20 15:43:48 +0200

    refactor DBs

---
+++ src/data/selectOptions.js
@@ -0,0 +1,62 @@
+export const careers = [
+  "Career 1",
+  "Career 2",
+  "Career 3",
+  "Career 4",
+  "Career 5",
+  "Career 6",
+  "Career 7",
+  "Career 8",
+  "Career 9",
+  "Career 10",
+];
+export const zodiacs = [
+  {
+    name: "Aries",
+    date: "Mar 21 - Apr 19",
+  },
+  {
+    name: "Taurus",
+    date: "Apr 20 - May 20",
+  },
+  {
+    name: "Gemini",
+    date: "May 21 - Jun 20",
+  },
+  {
+    name: "Cancer",
+    date: "Jun 21 - Jul 22",
+  },
+  {
+    name: "Leo",
+    date: "Jul 23 - Aug 22",
+  },
+  {
+    name: "Virgo",
+    date: "Aug 23 - Sep 22",
+  },
+  {
+    name: "Libra",
+    date: "Sep 23 - Oct 22",
+  },
+  {
+    name: "Scorpio",
+    date: "Oct 23 - Nov 21",
+  },
+  {
+    name: "Sagittarius",
+    date: "Nov 22 - Dec 21",
+  },
+  {
+    name: "Capricorn",
+    date: "Dec 22 - Jan 19",
+  },
+  {
+    name: "Aquarius",
+    date: "Jan 20 - Feb 18",
+  },
+  {
+    name: "Pisces",
+    date: "Feb 19 - Mar 20",
+  },
+];
